ac9bad6aa2fa05e7c6099b30299b8b192415939c
zyb <vzmc12@gmail.com>
2022-01-05 18:22:43 +0900

    first commit

---
+++ Glitter/Headers/glitter.hpp
@@ -0,0 +1,24 @@
+// Preprocessor Directives
+#ifndef GLITTER
+#define GLITTER
+#pragma once
+
+// System Headers
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
+#include <btBulletDynamicsCommon.h>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// Reference: https://github.com/nothings/stb/blob/master/stb_image.h#L4
+// To use stb_image, add this in *one* C++ source file.
+//     #define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+// Define Some Constants
+constexpr int mWidth = 1280;
+constexpr int mHeight = 800;
+
+#endif //~ Glitter Header
