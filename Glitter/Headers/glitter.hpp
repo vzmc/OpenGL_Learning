14a2bc6aa71719525dc0761e7296c4ea66b527f5
zyb <vzmc12@gmail.com>
2022-01-23 01:03:19 +0900

    fix

--- Glitter/Headers/glitter.hpp
+++ Glitter/Headers/glitter.hpp
@@ -19,17 +19,36 @@
 #include "GLFW/glfw3.h"
 
 // Define Some Constants
-constexpr int mWidth = 800;
-constexpr int mHeight = 600;
+constexpr int mWidth = 1200;
+constexpr int mHeight = 900;
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+inline void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     fprintf(stderr, "Viewport changed (%d, %d)\n", width, height);
     glViewport(0, 0, mWidth, mHeight);
 }
 
-void process_input(GLFWwindow* window) {
+inline void process_input(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, true);
+    }
+}
+
+int    nbFrames = 0;
+double lastTime = 0.0;
+inline void set_window_fps(GLFWwindow* win) {
+    // Measure speed
+    const double currentTime = glfwGetTime();
+    nbFrames++;
+
+    if (currentTime - lastTime >= 1.0) {
+        // If last cout was more than 1 sec ago
+        char title[256];
+        title[255] = '\0';
+        snprintf(title, 255, "%s %s - [FPS: %d]", "OpenGL", "4.0", nbFrames);
+        glfwSetWindowTitle(win, title);
+
+        nbFrames = 0;
+        lastTime += 1.0;
     }
 }
 
